c1ea9c85e239be83c2ee21f015eeed1393f14aa3
YuKi <1801154@s.asojuku.ac.jp>
2020-11-06 11:54:31 +0900

    ＃９

---
+++ app/src/main/java/com/example/snap_develop/activity/activity_comment.java
@@ -0,0 +1,14 @@
+package com.example.snap_develop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_comment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
