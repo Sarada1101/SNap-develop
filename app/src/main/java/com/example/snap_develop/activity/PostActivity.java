d674691eb4f85f3fbc92093318ae9aaf9cf89611
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-09 14:29:05 +0900

    update: DataBinding #25

--- app/src/main/java/com/example/snap_develop/activity/PostActivity.java
+++ app/src/main/java/com/example/snap_develop/activity/PostActivity.java
@@ -11,11 +11,13 @@ import com.example.snap_develop.viewModel.PostViewModel;
 
 public class PostActivity extends AppCompatActivity {
 
+    private ActivityPostBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         PostViewModel postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_post);
         PostBean postBean = new PostBean();
         postBean.setMessage("test");
         postViewModel.insertPost(postBean);
