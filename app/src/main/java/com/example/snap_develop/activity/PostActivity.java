5e13f272ecfa54cb207998b4df6ffa30f2448e2e
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-09 14:29:41 +0900

    update: OnClickListener #25

--- app/src/main/java/com/example/snap_develop/activity/PostActivity.java
+++ app/src/main/java/com/example/snap_develop/activity/PostActivity.java
@@ -11,6 +11,8 @@ import com.example.snap_develop.viewModel.PostViewModel;
 
 public class PostActivity extends AppCompatActivity {
 
+public class PostActivity extends AppCompatActivity implements View.OnClickListener {
+
     private ActivityPostBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +20,18 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
         PostViewModel postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_post);
+        mBinding.postFloatingActionButton.setOnClickListener(this);
         PostBean postBean = new PostBean();
         postBean.setMessage("test");
         postViewModel.insertPost(postBean);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.postFloatingActionButton) {
+            Log.d("insertPost", "success");
+            insertPost();
+        }
     }
 }
