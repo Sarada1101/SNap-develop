8ce7cff937eaf129bfa2f4b3c1201a43b07b7103
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-11 16:08:26 +0900

    fix: 重複を削除

--- app/src/main/java/com/example/snap_develop/model/FirestoreBase.java
+++ app/src/main/java/com/example/snap_develop/model/FirestoreBase.java
@@ -8,8 +8,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class FirestoreBase {
     protected FirebaseFirestore firestore = null;
 
-    protected FirebaseFirestore firestore = null;
-
     public void connect() {
         Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
         firestore = FirebaseFirestore.getInstance();
