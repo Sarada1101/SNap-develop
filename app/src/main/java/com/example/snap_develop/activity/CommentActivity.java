0f423283da8e6ddee16bbb68c2603e64190e0eaa
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-11 10:01:41 +0900

    update: import R

--- app/src/main/java/com/example/snap_develop/activity/CommentActivity.java
+++ app/src/main/java/com/example/snap_develop/activity/CommentActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.snap_develop.R;
+
 public class CommentActivity extends AppCompatActivity {
 
     @Override
