c39122bc7d66f5cdfbc135888c4eafd4d6b83469
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-11 20:07:05 +0900

    test

--- app/src/main/java/com/example/snap_develop/model/FirestoreBase.java
+++ app/src/main/java/com/example/snap_develop/model/FirestoreBase.java
@@ -3,7 +3,6 @@ package com.example.snap_develop.model;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FirestoreBase {
-
     protected FirebaseFirestore firestore = null;
 
     public void connect() {
