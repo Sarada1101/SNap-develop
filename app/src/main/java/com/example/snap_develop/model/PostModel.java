b80acd2d0c8190594f61a580cd778a2b16844217
kaito <1801193aso@gmail.com>
2020-11-11 15:26:19 +0900

    タスク_コメント投稿機能#19
    タスク_コメント投稿機能の追加

--- app/src/main/java/com/example/snap_develop/model/PostModel.java
+++ app/src/main/java/com/example/snap_develop/model/PostModel.java
@@ -1,4 +1,71 @@
 package com.example.snap_develop.model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.snap_develop.bean.PostBean;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PostModel extends FirestoreBase {
+
+    private final String TAG = "Firestore";
+    String postID = "";
+
+    public void insertPost(PostBean postBean) {
+        Map<String, Object> post = new HashMap<>();
+        post.put("message", postBean.getMessage());
+        post.put("picture", postBean.getPicture());
+        //TODO 現在地
+        post.put("datetime", postBean.getDatetime());
+        post.put("anonymous", postBean.isAnonymous());
+        post.put("danger", postBean.isDanger());
+        post.put("uid", postBean.getUid());
+        post.put("type", postBean.getType());
+
+        this.connect();
+
+        firestore.collection("posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        postID = documentReference.getId();
+                        System.out.println(
+                                "Documentwritten with ID: " + postID);
+                        Log.d(TAG,
+                                "Document written with ID: " + postID);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        //TODO usersコレクションにパスを挿入
+        firestore.collection("users")
+                .document(postBean.getUid())
+                .collection("posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        Log.d(TAG,
+                                "Document written with ID: " + postID);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/snap_develop/viewModel/PostViewModel.java
+++ app/src/main/java/com/example/snap_develop/viewModel/PostViewModel.java
@@ -2,5 +2,14 @@ package com.example.snap_develop.viewModel;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.snap_develop.bean.PostBean;
+import com.example.snap_develop.model.PostModel;
+
 public class PostViewModel extends ViewModel {
+    PostModel postModel = new PostModel();
+
+    public void insertComment(PostBean postBean) {
+        postModel.insertComment(postBean);
+    }
+
 }
