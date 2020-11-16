f3e4aa6a658a7fb0a31c0571047eae6528f3b79e
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-16 15:03:23 +0900

    update: ログ出力 #40

--- app/src/main/java/com/example/snap_develop/model/MapModel.java
+++ app/src/main/java/com/example/snap_develop/model/MapModel.java
@@ -1,17 +1,14 @@
 package com.example.snap_develop.model;
 
 import android.annotation.SuppressLint;
-import android.graphics.Point;
 import android.location.Location;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.snap_develop.util.LogUtil;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -23,38 +20,27 @@ import java.util.Map;
 
 public class MapModel {
 
-    public LatLng deviceLocation = new LatLng(0, 0);
     private Location location;
 
     @SuppressLint("MissingPermission")
     public void getDeviceLocation(FusedLocationProviderClient fusedLocationClient,
-            final GoogleMap mMap, final MutableLiveData<LatLng> deviceLocationResult) {
-
-        final double[] latitude = {0};
-        final double[] longitude = {0};
-
+            final MutableLiveData<LatLng> deviceLatLng) {
+        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
         Task<Location> getLastLocation = fusedLocationClient.getLastLocation();
-        getLastLocation.addOnCompleteListener(
-                new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful() && task.getResult() != null) {
-                            location = task.getResult();
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                            System.out.println(
-                                    "-------------------------getLastLocation:TRUE"
-                                            + "---------------------------");
-                            System.out.println("緯度：" + latitude + "経度：" + longitude);
-                        } else {
-                            System.out.println(
-                                    "-------------------------getLastLocation:ELSE"
-                                            + "---------------------------");
-                            Log.d("debug", "計測不能");
-                        }
-                    }
-                });
-        return new LatLng(latitude, longitude);
+        getLastLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    location = task.getResult();
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    Log.i(LogUtil.getClassName(),
+                            String.format("lat:%s , lon:%s", latLng.latitude, latLng.longitude));
+                    deviceLatLng.setValue(latLng);
+                } else {
+                    Log.d(LogUtil.getClassName(), String.valueOf(task.getException()));
+                }
+            }
+        });
     }
 
     public int getRadius(GoogleMap googleMap) {
@@ -96,7 +82,7 @@ public class MapModel {
                 result = radius.get(key.getKey() - 1);
             }
         }
-        Log.d(TAG, String.format("zoomLevel:%s , radius:%sm", zoom, result));
+        Log.d(LogUtil.getClassName(), String.format("zoomLevel:%s , radius:%sm", zoom, result));
         return result;
     }
 
@@ -104,7 +90,8 @@ public class MapModel {
         Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
         CameraPosition cameraPos = googleMap.getCameraPosition();
         LatLng centerLatLon = new LatLng(cameraPos.target.latitude, cameraPos.target.longitude);
-        Log.d(TAG, String.format("lat:%s , lon:%s", centerLatLon.latitude, centerLatLon.longitude));
+        Log.d(LogUtil.getClassName(),
+                String.format("lat:%s , lon:%s", centerLatLon.latitude, centerLatLon.longitude));
         return new double[]{centerLatLon.latitude, centerLatLon.longitude};
     }
 }
