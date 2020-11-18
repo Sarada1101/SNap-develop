5e3fcd4fbb39bbe8a3bac9c522371c755e71b07b
kaito <1801193aso@gmail.com>
2020-11-18 13:40:15 +0900

    メソッド名の変更

--- app/src/main/java/com/example/snap_develop/viewModel/PostViewModel.java
+++ app/src/main/java/com/example/snap_develop/viewModel/PostViewModel.java
@@ -1,19 +1,16 @@
 package com.example.snap_develop.viewModel;
 
-import android.util.Log;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.snap_develop.bean.PostBean;
 import com.example.snap_develop.model.PostModel;
-import com.example.snap_develop.util.LogUtil;
 
 public class PostViewModel extends ViewModel {
     MutableLiveData<PostBean> post;
     PostModel postModel = new PostModel();
 
     public void insertPost(PostBean postBean) {
-        postModel.insertComment(postBean);
+        postModel.insertPost(postBean);
     }
 }
