dd036069c7a2e92237128909d1a3287253f49abd
Nishimura <1901335@s.asojuku.ac.jp>
2020-11-26 11:31:29 +0900

    #43
    FollowingListActivity修正

--- app/src/main/java/com/example/snap_develop/activity/FollowingListActivity.java
+++ app/src/main/java/com/example/snap_develop/activity/FollowingListActivity.java
@@ -1,39 +1,22 @@
 package com.example.snap_develop.activity;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.snap_develop.R;
-import com.example.snap_develop.bean.UserBean;
-import com.example.snap_develop.model.FollowListAdapter;
 import com.example.snap_develop.util.LogUtil;
-import com.example.snap_develop.viewModel.FollowViewModel;
-import com.example.snap_develop.viewModel.UserViewModel;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class FollowingListActivity extends AppCompatActivity {
     ListView lv;
-    FollowListAdapter fAdapter;
-    ArrayList<HashMap<String, Object>> listData;
-    FollowViewModel followViewModel;
-    UserViewModel userViewModel;
-    Integer followCount;
-    int count = 0;
-    int count2 = 0;
-    List<UserBean> dispFollowList;
-    FirebaseUser currentUser;
-    String currentUid;
+    SimpleAdapter sAdapter;
+    ArrayList<HashMap<String, String>> listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,65 +24,23 @@ public class FollowingListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_following_list);
 
-        listData = new ArrayList<HashMap<String, Object>>();
+        listData = new ArrayList<HashMap<String, String>>();
 
-        followViewModel = new ViewModelProvider(this).get(FollowViewModel.class);
-        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        HashMap<String, String> data1 = new HashMap<String, String>();
+        data1.put("username", "西山大成");
+        data1.put("userid", "t1233");
+        listData.add(data1);
 
-        //現在ログイン中のユーザーのUidを取得する処理
-        //currentUser = userViewModel.getCurrentUser();
-        //currentUid = currentUser.getUid();
+        HashMap<String, String> data2 = new HashMap<String, String>();
+        data2.put("username", "井上r");
+        data2.put("userid", "ijdsbf");
+        listData.add(data2);
 
-        //テストデータ
-        currentUid = "UtJFmruiiBS28WH333AE6YHEjf72";
-
-        //フォローしている人数を取得する処理
-        followViewModel.fetchCount(currentUid, "follower_count");
-
-        followViewModel.getUserCount().observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(Integer userCount) {
-                System.out.println(userCount);
-                followCount = userCount;
-                followViewModel.fetchFollowingList(currentUid);
-            }
-        });
-
-        followViewModel.getFollowList().observe(this, new Observer<List<UserBean>>() {
-            @Override
-            public void onChanged(List<UserBean> followList) {
-                System.out.println("--------------------onChanged:count----------------------");
-                count++;
-                if (count >= followCount) {
-                    dispFollowList = new ArrayList<>();
-                    dispFollowList = followList;
-                    userViewModel.fetchIconBmp(dispFollowList);
-                }
-
-            }
-        });
-
-        userViewModel.getIconList().observe(this, new Observer<Map<String, Bitmap>>() {
-            @Override
-            public void onChanged(Map<String, Bitmap> iconList) {
-                System.out.println("--------------------onChanged----------------------");
-                count2++;
-                if (count2 >= dispFollowList.size()) {
-                    System.out.println("---------------" + count + "+" + dispFollowList.size() + "----------------------");
-                    for (UserBean bean : dispFollowList) {
-                        HashMap<String, Object> addData = new HashMap<String, Object>();
-                        addData.put("username", bean.getName());
-                        addData.put("userid", bean.getUid());
-                        addData.put("usericon", iconList.get(bean.getUid()));
-                        System.out.println(iconList);
-                        listData.add(addData);
-                    }
-                    fAdapter = new FollowListAdapter(FollowingListActivity.this, listData, R.layout.activity_following_list_row);
-                    lv = (ListView) findViewById(R.id.followView);
-                    lv.setAdapter(fAdapter);
-                }
-            }
-        });
+        sAdapter = new SimpleAdapter(this, listData, R.layout.activity_following_list_row,
+                new String[]{"usericon", "username", "userid"},
+                new int[]{R.id.iconImageView, R.id.nameTextView, R.id.idTextView});
+        lv = (ListView) findViewById(R.id.followView);
+        lv.setAdapter(sAdapter);
     }
 
 }
