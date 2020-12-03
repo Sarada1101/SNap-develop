57527cb8721d123231de76f494bdc0b787835679
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-03 11:54:32 +0900

    変数を初期化する

--- app/src/main/java/com/example/snap_develop/viewModel/FollowViewModel.java
+++ app/src/main/java/com/example/snap_develop/viewModel/FollowViewModel.java
@@ -1,43 +1,54 @@
 package com.example.snap_develop.viewModel;
 
-import android.util.Log;
-
 import androidx.lifecycle.ViewModel;
 
+import com.example.snap_develop.MyDebugTree;
 import com.example.snap_develop.model.FollowModel;
-import com.example.snap_develop.util.LogUtil;
+
+import timber.log.Timber;
 
 public class FollowViewModel extends ViewModel {
 
     FollowModel followModel = new FollowModel();
 
     public void deleteApplicatedFollow(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.deleteApplicatedFollow(userPath, myUid);
     }
 
+
     public void deleteApprovalPendingFollow(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.deleteApprovalPendingFollow(userPath, myUid);
     }
 
+
     public void insertFollower(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.insertFollower(userPath, myUid);
     }
 
+
     public void insertFollowing(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.insertFollowing(userPath, myUid);
     }
 
+
     public void insertApplicatedFollow(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.insertApplicatedFollow(userPath, myUid);
     }
 
+
     public void insertApprovalPendingFollow(String userPath, String myUid) {
-        Log.i(LogUtil.getClassName(), LogUtil.getLogMessage());
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "userPath", userPath, "myUid", myUid));
         followModel.insertApprovalPendingFollow(userPath, myUid);
     }
 }
