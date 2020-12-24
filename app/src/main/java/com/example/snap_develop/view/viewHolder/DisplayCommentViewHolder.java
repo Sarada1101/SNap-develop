1df908c1535e21dd7d0a3745cb8faa2138ef3a92
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-24 12:22:18 +0900

    chore: DisplayCommentViewHolder作成
    #191

---
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/DisplayCommentViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.snap_develop.view.viewHolder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DisplayCommentViewHolder extends RecyclerView.ViewHolder {
+    public DisplayCommentViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
