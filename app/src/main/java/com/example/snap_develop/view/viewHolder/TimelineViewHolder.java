97e7ba27d7a8e46cca74f6d0813e348d454dc69b
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-25 12:20:37 +0900

    feat(TimelineViewHolder): 画面との紐付け
    #191

--- app/src/main/java/com/example/snap_develop/view/viewHolder/TimelineViewHolder.java
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/TimelineViewHolder.java
@@ -1,4 +1,38 @@
 package com.example.snap_develop.view.viewHolder;
 
-public class TimelineViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snap_develop.R;
+
+public class TimelineViewHolder extends RecyclerView.ViewHolder {
+    public ImageView mIconImageView;
+    public ImageView mPhotoImageView;
+    public TextView mUserNameTextView;
+    public TextView mUserIdTextView;
+    public TextView mTimelineTextView;
+    public TextView mGoodCountTextView;
+    public TextView mLatLngTextView;
+    public TextView mDatetimeTextView;
+    public ConstraintLayout mConstraintLayout;
+    public ConstraintLayout mUserInfoConstraintLayout;
+
+    public TimelineViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mIconImageView = itemView.findViewById(R.id.iconImageView);
+        mPhotoImageView = itemView.findViewById(R.id.photoImageView);
+        mUserNameTextView = itemView.findViewById(R.id.userNameTextView);
+        mUserIdTextView = itemView.findViewById(R.id.userIdTextView);
+        mTimelineTextView = itemView.findViewById(R.id.timelineTextView);
+        mGoodCountTextView = itemView.findViewById(R.id.goodCountTextView);
+        mLatLngTextView = itemView.findViewById(R.id.latLngTextView);
+        mDatetimeTextView = itemView.findViewById(R.id.datetimeTextView);
+        mConstraintLayout = itemView.findViewById(R.id.ConstraintLayout);
+        mUserInfoConstraintLayout = itemView.findViewById(R.id.userInfoConstraintLayout);
+    }
 }
