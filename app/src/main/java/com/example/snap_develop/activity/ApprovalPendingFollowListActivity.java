3c1acb7d68d1824075522caa6475e5976fc5bcd9
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 22:02:09 +0900

    feat: RecyclerViewを表示する処理
    #191

--- app/src/main/java/com/example/snap_develop/activity/ApprovalPendingFollowListActivity.java
+++ app/src/main/java/com/example/snap_develop/activity/ApprovalPendingFollowListActivity.java
@@ -3,13 +3,14 @@ package com.example.snap_develop.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.snap_develop.MyDebugTree;
 import com.example.snap_develop.R;
@@ -23,14 +24,13 @@ import java.util.List;
 
 import timber.log.Timber;
 
-public class ApprovalPendingFollowListActivity extends AppCompatActivity implements View.OnClickListener,
-        AdapterView.OnItemClickListener {
+public class ApprovalPendingFollowListActivity extends AppCompatActivity implements View.OnClickListener {
 
     private UserViewModel mUserViewModel;
     private FollowViewModel mFollowViewModel;
     private ActivityApprovalPendingFollowListBinding mBinding;
     private ApprovalPendingFollowListAdapter mApprovalPendingFollowListAdapter;
-    private ListView mListView;
+    private RecyclerView mRecyclerView;
     private List<UserBean> mFollowList;
     private String mUid;
 
@@ -38,7 +38,7 @@ public class ApprovalPendingFollowListActivity extends AppCompatActivity impleme
     protected void onCreate(Bundle savedInstanceState) {
         Timber.i(MyDebugTree.START_LOG);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_applicated_follow_list);
+        setContentView(R.layout.activity_approval_pending_follow_list);
         setTitle("フォロー承認待ち");
 
         mFollowViewModel = new ViewModelProvider(this).get(FollowViewModel.class);
@@ -46,7 +46,7 @@ public class ApprovalPendingFollowListActivity extends AppCompatActivity impleme
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_approval_pending_follow_list);
 
         mBinding.applicatedFollowButton.setOnClickListener(this);
-        mBinding.approvalPendingFolliwButton.setOnClickListener(this);
+        mBinding.approvalPendingFollowButton.setOnClickListener(this);
         mBinding.timelineImageButton.setOnClickListener(this);
         mBinding.mapImageButton.setOnClickListener(this);
         mBinding.userImageButton.setOnClickListener(this);
@@ -58,13 +58,18 @@ public class ApprovalPendingFollowListActivity extends AppCompatActivity impleme
                 Timber.i(MyDebugTree.START_LOG);
                 Timber.i(String.format("%s %s=%s", MyDebugTree.INPUT_LOG, "followList", followList));
 
-                mFollowList = followList;
                 mApprovalPendingFollowListAdapter = new ApprovalPendingFollowListAdapter(
-                        ApprovalPendingFollowListActivity.this,
-                        followList, R.layout.activity_approval_pending_follow_list_row);
-                mListView = mBinding.approvalPendingFollowListView;
-                mListView.setAdapter(mApprovalPendingFollowListAdapter);
-                mListView.setOnItemClickListener(ApprovalPendingFollowListActivity.this);
+                        ApprovalPendingFollowListActivity.this, followList);
+                mRecyclerView = mBinding.approvalPendingFollowRecyclerView;
+                LinearLayoutManager llm = new LinearLayoutManager(ApprovalPendingFollowListActivity.this);
+                mRecyclerView.setLayoutManager(llm);
+                RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(
+                        ApprovalPendingFollowListActivity.this, DividerItemDecoration.VERTICAL);
+                mRecyclerView.addItemDecoration(itemDecoration);
+                mRecyclerView.setAdapter(mApprovalPendingFollowListAdapter);
+
+                mFollowList = followList;
+                mApprovalPendingFollowListAdapter.setOnItemClickListener(ApprovalPendingFollowListActivity.this);
             }
         });
 
@@ -76,6 +81,13 @@ public class ApprovalPendingFollowListActivity extends AppCompatActivity impleme
     private void cancelFollow(String uid, String myUid) {
         mFollowViewModel.deleteApplicatedFollow(/*from*/uid, /*delete*/myUid);
         mFollowViewModel.deleteApprovalPendingFollow(myUid, uid);
+    }
+
+
+    private void listRemove(int position) {
+        mFollowList.remove(position);
+        mBinding.approvalPendingFollowRecyclerView.getAdapter().notifyItemRemoved(position);
+        mBinding.approvalPendingFollowRecyclerView.getAdapter().notifyItemRangeRemoved(position, mFollowList.size());
     }
 
 
@@ -92,25 +104,12 @@ public class ApprovalPendingFollowListActivity extends AppCompatActivity impleme
             startActivity(new Intent(getApplication(), UserActivity.class));
         } else if (i == R.id.applicatedFollowButton) {
             startActivity(new Intent(getApplication(), ApplicatedFollowListActivity.class));
-        } else if (i == R.id.approvalPendingFolliwButton) {
+        } else if (i == R.id.approvalPendingFollowButton) {
             startActivity(new Intent(getApplication(), ApprovalPendingFollowListActivity.class));
-        }
-    }
-
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (view.getId() == R.id.rejectButton) {
+        } else if (i == R.id.rejectButton) {
+            int position = mApprovalPendingFollowListAdapter.mPosition;
             cancelFollow(mFollowList.get(position).getUid(), mUid);
-
-            mFollowList.remove(position);
-            mApprovalPendingFollowListAdapter = new ApprovalPendingFollowListAdapter(
-                    ApprovalPendingFollowListActivity.this,
-                    mFollowList, R.layout.activity_approval_pending_follow_list_row);
-            mListView.setAdapter(mApprovalPendingFollowListAdapter);
-        } else {
-            UserBean userBean = mFollowList.get(position);
-            startActivity(new Intent(getApplication(), UserActivity.class).putExtra("uid", userBean.getUid()));
+            listRemove(position);
         }
     }
 }
