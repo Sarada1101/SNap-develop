5a6a645dfbd9d9cdc1b55b39e91f0bff89c24dbd
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 11:36:43 +0900

    feat: RecyclerViewを表示する処理
    #191

--- app/src/main/java/com/example/snap_develop/view/adapter/UserAdapter.java
+++ app/src/main/java/com/example/snap_develop/view/adapter/UserAdapter.java
@@ -1,104 +1,97 @@
 package com.example.snap_develop.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.snap_develop.MyDebugTree;
 import com.example.snap_develop.R;
+import com.example.snap_develop.activity.DisplayCommentActivity;
 import com.example.snap_develop.bean.PostBean;
 import com.example.snap_develop.bean.UserBean;
+import com.example.snap_develop.view.viewHolder.UserViewHolder;
 
 import java.util.List;
 
 import timber.log.Timber;
 
-public class UserAdapter extends BaseAdapter {
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
 
-    private List<PostBean> mPostList;
+    private Context mContext;
+    private List<PostBean> mPostBeanList;
     private UserBean mUserBean;
-    private LayoutInflater mInflater;
-    private int mLayoutID;
 
-    static class ViewHolder {
-        ImageView icon;
-        TextView username;
-        TextView uid;
-        TextView post;
-        ImageView photo;
-        TextView goodCount;
-        TextView latLng;
-        TextView datetime;
-    }
-
-
-    public UserAdapter(Context context, List<PostBean> postList, UserBean userBean, int rowLayout) {
+    public UserAdapter(Context context, List<PostBean> postList, UserBean userBean) {
         Timber.i(MyDebugTree.START_LOG);
-        this.mPostList = postList;
+        Timber.i(String.format("%s %s=%s, %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "context", context, "postList",
+                postList, "userBean", userBean));
+
+        this.mContext = context;
+        this.mPostBeanList = postList;
         this.mUserBean = userBean;
-        this.mInflater = LayoutInflater.from(context);
-        this.mLayoutID = rowLayout;
     }
 
 
+    @NonNull
     @Override
-    public int getCount() {
-        return this.mPostList.size();
-    }
-
-
-    @Override
-    public Object getItem(int position) {
-        return position;
-    }
-
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Timber.i(MyDebugTree.START_LOG);
-        Timber.i(String.format("%s %s=%s, %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "position", position, "convertView",
-                convertView, "parent", parent));
-        ViewHolder holder;
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "parent", parent, "viewType", viewType));
 
-        if (convertView == null) {
-            convertView = mInflater.inflate(mLayoutID, null);
-            holder = new ViewHolder();
-            holder.icon = convertView.findViewById(R.id.listIconImageView);
-            holder.username = convertView.findViewById(R.id.listUserNameTextView);
-            holder.uid = convertView.findViewById(R.id.listUserIdTextView);
-            holder.post = convertView.findViewById(R.id.listCommentTextView);
-            holder.photo = convertView.findViewById(R.id.photoImageView);
-            holder.goodCount = convertView.findViewById(R.id.goodCountTextView);
-            holder.latLng = convertView.findViewById(R.id.latLngTextView);
-            holder.datetime = convertView.findViewById(R.id.dateTextView);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_user_item, parent,
+                false);
+        return new UserViewHolder(inflate);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        Timber.i(MyDebugTree.START_LOG);
+        Timber.i(String.format("%s %s=%s, %s=%s", MyDebugTree.INPUT_LOG, "holder", holder, "position", position));
+
+        final PostBean postBean = mPostBeanList.get(position);
+        holder.mIconImageView.setImageBitmap(mUserBean.getIcon());
+        holder.mUserNameTextView.setText(mUserBean.getName());
+        holder.mUserIdTextView.setText(mUserBean.getUid());
+        holder.mMessageTextView.setText(postBean.getMessage());
+        holder.mDatetimeTextView.setText(postBean.getStrDatetime());
+
+        if (postBean.getPhoto() != null) holder.mPhotoImageView.setImageBitmap(postBean.getPhoto());
+
+        if (postBean.getType().equals("post")) {
+            holder.mGoodCountTextView.setText(Integer.toString(postBean.getGoodCount()));
+            holder.mLatLngTextView.setText(
+                    String.format("%d, %d", (int) postBean.getLatLng().latitude, (int) postBean.getLatLng().longitude));
         }
 
-        holder.icon.setImageBitmap(mUserBean.getIcon());
-        holder.username.setText(mUserBean.getName());
-        holder.uid.setText(mUserBean.getUid());
-        holder.post.setText(mPostList.get(position).getMessage());
-        holder.datetime.setText(mPostList.get(position).getStrDatetime());
-
-        if (mPostList.get(position).getPhoto() != null) holder.photo.setImageBitmap(mPostList.get(position).getPhoto());
-
-        if (mPostList.get(position).getType().equals("post")) {
-            holder.goodCount.setText(Integer.toString(mPostList.get(position).getGoodCount()));
-            holder.latLng.setText(String.format("%d, %d", (int) mPostList.get(position).getLatLng().latitude,
-                    (int) mPostList.get(position).getLatLng().longitude));
+        if (postBean.isDanger()) {
+            holder.mConstraintLayout.setBackgroundColor(Color.rgb(240, 96, 96));
         }
-        return convertView;
+
+        holder.mConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (postBean.getType().equals("post")) {
+                    mContext.startActivity(new Intent(mContext, DisplayCommentActivity.class).putExtra("postPath",
+                            postBean.getPostPath()));
+                } else if (postBean.getType().equals("comment")) {
+                    mContext.startActivity(new Intent(mContext, DisplayCommentActivity.class).putExtra("postPath",
+                            postBean.getParentPost()));
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        Timber.i(MyDebugTree.START_LOG);
+        return mPostBeanList.size();
     }
 }
