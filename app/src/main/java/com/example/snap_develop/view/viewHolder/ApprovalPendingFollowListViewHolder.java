0bcb67befeadcd48c3fd7e22cd1001edf5ce4f19
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 22:01:37 +0900

    feat(ApprovalPendingFollowListViewHolder): 画面と紐付け
    #191

--- app/src/main/java/com/example/snap_develop/view/viewHolder/ApprovalPendingFollowListViewHolder.java
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/ApprovalPendingFollowListViewHolder.java
@@ -1,4 +1,30 @@
 package com.example.snap_develop.view.viewHolder;
 
-public class ApprovalPendingFollowListViewHolder {
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snap_develop.R;
+
+public class ApprovalPendingFollowListViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView mIconImageView;
+    public TextView mUserNameTextView;
+    public TextView mUserIdTextView;
+    public Button mRejectButton;
+    public ConstraintLayout mConstraintLayout;
+
+    public ApprovalPendingFollowListViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mIconImageView = itemView.findViewById(R.id.iconImageView);
+        mUserNameTextView = itemView.findViewById(R.id.userNameTextView);
+        mUserIdTextView = itemView.findViewById(R.id.userIdTextView);
+        mRejectButton = itemView.findViewById(R.id.rejectButton);
+        mConstraintLayout = itemView.findViewById(R.id.ConstraintLayout);
+    }
 }
