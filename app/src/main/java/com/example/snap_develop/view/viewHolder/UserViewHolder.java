675d920d432573372851e7a0ef4a4a080c23744d
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 11:36:14 +0900

    feat(UserViewHolder): 画面と紐付け
    #191

--- app/src/main/java/com/example/snap_develop/view/viewHolder/UserViewHolder.java
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/UserViewHolder.java
@@ -1,4 +1,37 @@
 package com.example.snap_develop.view.viewHolder;
 
-public class UserViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snap_develop.R;
+
+public class UserViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView mIconImageView;
+    public ImageView mPhotoImageView;
+    public TextView mUserNameTextView;
+    public TextView mUserIdTextView;
+    public TextView mMessageTextView;
+    public TextView mGoodCountTextView;
+    public TextView mLatLngTextView;
+    public TextView mDatetimeTextView;
+    public ConstraintLayout mConstraintLayout;
+
+    public UserViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mIconImageView = itemView.findViewById(R.id.iconImageView);
+        mPhotoImageView = itemView.findViewById(R.id.photoImageView);
+        mUserNameTextView = itemView.findViewById(R.id.userNameTextView);
+        mUserIdTextView = itemView.findViewById(R.id.userIdTextView);
+        mMessageTextView = itemView.findViewById(R.id.messageTextView);
+        mGoodCountTextView = itemView.findViewById(R.id.goodCountTextView);
+        mLatLngTextView = itemView.findViewById(R.id.latLngTextView);
+        mDatetimeTextView = itemView.findViewById(R.id.datetimeTextView);
+        mConstraintLayout = itemView.findViewById(R.id.ConstraintLayout);
+    }
 }
