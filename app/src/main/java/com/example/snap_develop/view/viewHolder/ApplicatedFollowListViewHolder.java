1b1ffba741196c27e1f9aabc9f949e635be00898
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 21:18:49 +0900

    feat(ApplicaredFollowListViewHolder): 画面と紐付け

--- app/src/main/java/com/example/snap_develop/view/viewHolder/ApplicatedFollowListViewHolder.java
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/ApplicatedFollowListViewHolder.java
@@ -1,4 +1,32 @@
 package com.example.snap_develop.view.viewHolder;
 
-public class ApplicatedFollowListViewHolder {
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snap_develop.R;
+
+public class ApplicatedFollowListViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView mIconImageView;
+    public TextView mUserNameTextView;
+    public TextView mUserIdTextView;
+    public Button mApprovalButton;
+    public Button mRejectButton;
+    public ConstraintLayout mConstraintLayout;
+
+    public ApplicatedFollowListViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mIconImageView = itemView.findViewById(R.id.iconImageView);
+        mUserNameTextView = itemView.findViewById(R.id.userNameTextView);
+        mUserIdTextView = itemView.findViewById(R.id.userIdTextView);
+        mApprovalButton = itemView.findViewById(R.id.approvalButton);
+        mRejectButton = itemView.findViewById(R.id.rejectButton);
+        mConstraintLayout = itemView.findViewById(R.id.ConstraintLayout);
+    }
 }
