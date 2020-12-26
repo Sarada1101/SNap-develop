7a910839746688bcc793e769b1200194f2ac534d
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-26 13:08:53 +0900

    feat(FollowListViewHolder): 画面と紐付け
    #191

--- app/src/main/java/com/example/snap_develop/view/viewHolder/FollowListViewHolder.java
+++ app/src/main/java/com/example/snap_develop/view/viewHolder/FollowListViewHolder.java
@@ -1,4 +1,27 @@
 package com.example.snap_develop.view.viewHolder;
 
-public class FollowListViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snap_develop.R;
+
+public class FollowListViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView mIconImageView;
+    public TextView mUserNameTextView;
+    public TextView mUserIdTextView;
+    public ConstraintLayout mConstraintLayout;
+
+    public FollowListViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mIconImageView = itemView.findViewById(R.id.iconImageView);
+        mUserNameTextView = itemView.findViewById(R.id.userNameTextView);
+        mUserIdTextView = itemView.findViewById(R.id.userIdTextView);
+        mConstraintLayout = itemView.findViewById(R.id.ConstraintLayout);
+    }
 }
