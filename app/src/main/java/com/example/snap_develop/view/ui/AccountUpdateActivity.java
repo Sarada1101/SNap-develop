e2612ef96b7688ba1854676e6a4aba26e4e9c612
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-01-05 13:54:44 +0900

    feat: アカウント情報変更画面作成
    #223

---
+++ app/src/main/java/com/example/snap_develop/view/ui/AccountUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.snap_develop.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_update);
+    }
+}
